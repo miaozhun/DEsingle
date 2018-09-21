8b4a15dd794a659bb67a7d00d10a76818d043218
miaozhun <mz8@foxmail.com>
2018-09-21 17:35:04 +0800

    Update DEtype.R

--- R/DEtype.R
+++ R/DEtype.R
@@ -64,11 +64,11 @@ DEtype <- function(results, threshold){
     stop("Invalid input of wrong data type of results")
   if(ncol(results) != 22)
     stop("Invalid input of wrong column number of results")
-  if(colnames(results)[21] != "pvalue.adj.FDR" | colnames(results)[18] != "FDR_LR2" | colnames(results)[19] != "FDR_LR3")
+  if(colnames(results)[21] != "pvalue.adj.FDR" | colnames(results)[16] != "pvalue_LR2" | colnames(results)[17] != "pvalue_LR3")
     stop("Invalid input of wrong column name of results")
   if(class(threshold) != "numeric")
     stop("Invalid input of wrong data type of threshold")
-  if(threshold <= 0 | threshold >= 1)
+  if(threshold <= 0 | threshold > 0.1)
     stop("Invalid input of wrong range of threshold")
 
   # Classify the types of DE genes
@@ -77,21 +77,21 @@ DEtype <- function(results, threshold){
   for(i in 1:nrow(results)){
     if(results[i,"pvalue.adj.FDR"] < threshold)
     {
-      if(results[i,"FDR_LR2"] < threshold & results[i,"FDR_LR3"] < threshold){
+      if(results[i,"pvalue_LR2"] < threshold & results[i,"pvalue_LR3"] < threshold){
         results[i,"Type"] <- "DEg"
         if(results[i,"mu_1"] * (1 - results[i,"theta_1"]) >= results[i,"mu_2"] * (1 - results[i,"theta_2"]))
           results[i,"State"] <- "up"
         else
           results[i,"State"] <- "down"
       }
-      else if(results[i,"FDR_LR2"] < threshold){
+      else if(results[i,"pvalue_LR2"] < threshold){
         results[i,"Type"] <- "DEs"
         if(results[i,"theta_1"] <= results[i,"theta_2"])
           results[i,"State"] <- "up"
         else
           results[i,"State"] <- "down"
       }
-      else if(results[i,"FDR_LR3"] < threshold){
+      else if(results[i,"pvalue_LR3"] < threshold){
         results[i,"Type"] <- "DEa"
         if(results[i,"mu_1"] >= results[i,"mu_2"])
           results[i,"State"] <- "up"
